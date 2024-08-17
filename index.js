250b9f5f2bc65b6a42165628116efc55e5558f19
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-08-17 12:56:42 +0530

    initial small express server setup for testing

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ sucess: true, message: "Welcome to the alanced home" });
+});
+app.get("/about", (req, res) => {
+  res
+    .status(200)
+    .json({ sucess: true, message: "Welcome to the alanced About page" });
+});
+app.get("/contact", (req, res) => {
+  res
+    .status(200)
+    .json({ sucess: true, message: "Welcome to the alanced Contact" });
+});
+
+app.listen(3000, () => {
+  console.log(`server is running at port :3000`);
+});
